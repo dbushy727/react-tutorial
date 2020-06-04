bf1b8bd8bf9cd1fb6665d36cdc8d0328b15d23a1
Daniel Bushkanets <d.bushkanets@gmail.com>
2020-06-04 09:10:15 -0700

    emotion demo

--- src/Content.js
+++ src/Content.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import styled from '@emotion/styled'
 
+const Text = styled.div`
+	background-color: green;
+`;
 
 function Content() {
   return (
-    <div>This is my Content!</div>
+    <Text>This is my Content!</Text>
   );
 }
 
